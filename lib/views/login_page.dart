02da7e1b55c3d33c8c91aefe528b36f3a65b4d07
ReinanHS <reinangabriel1520@gmail.com>
2020-10-21 15:30:24 -0300

    Stack

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -13,6 +13,55 @@ class LoginPageState extends State<LoginPage> {
   String email;
   String password;
 
+  Widget _body() {
+    return SingleChildScrollView(
+      child: Column(children: [
+        SizedBox(
+            width: 150, child: Image.asset('assets/images/example-logo.png')),
+        Container(
+          padding: EdgeInsets.all(40),
+          child: Text(
+            'Login no Sistema',
+            style: TextStyle(
+                fontSize: 34, fontWeight: FontWeight.w700, color: Colors.white),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (text) {
+                email = text;
+              },
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Email")),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: TextField(
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+              onChanged: (text) {
+                password = text;
+              },
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Senha")),
+        ),
+        Padding(
+            padding: EdgeInsets.all(10),
+            child: RaisedButton(
+              onPressed: () {
+                if (email == 'admin@gmail.com' && password == '123456') {
+                  Navigator.of(context).pushReplacementNamed('/home');
+                } else
+                  print('501- Error');
+              },
+              child: Text('Fazer Login'),
+            ))
+      ]),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,50 +70,22 @@ class LoginPageState extends State<LoginPage> {
         title: Text('Login'),
         leading: Icon(Icons.login),
       ),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          SizedBox(
-              width: 150, child: Image.asset('assets/images/example-logo.png')),
+      body: Stack(
+        children: [
           Container(
-            padding: EdgeInsets.all(40),
-            child: Text(
-              'Login no Sistema',
-              style: TextStyle(fontSize: 34, fontWeight: FontWeight.w700),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/lake-5011715_1920.jpg',
+              fit: BoxFit.cover,
             ),
           ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (text) {
-                  email = text;
-                },
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), labelText: "Email")),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            color: Colors.black.withOpacity(0.3),
           ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: TextField(
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                onChanged: (text) {
-                  password = text;
-                },
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), labelText: "Senha")),
-          ),
-          Padding(
-              padding: EdgeInsets.all(10),
-              child: RaisedButton(
-                onPressed: () {
-                  if (email == 'admin@gmail.com' && password == '123456') {
-                    Navigator.of(context).pushReplacementNamed('/home');
-                  } else
-                    print('501- Error');
-                },
-                child: Text('Fazer Login'),
-              ))
-        ]),
+          _body(),
+        ],
       ),
     );
   }

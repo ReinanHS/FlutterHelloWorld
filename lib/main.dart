5c3e4f760baea37c53cd331d3b9c1d306c29aba9
ReinanHS <reinangabriel1520@gmail.com>
2020-10-20 22:46:26 -0300

    StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,25 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() {
-  return runApp(Container(
-    child: Center(
-      child: Text(
-        "Hello World",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.blue, fontSize: 74.0),
-      ),
-    ),
+  return runApp(AppWidget(
+    title: "Hello World",
   ));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({Key key, this.title}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 75),
+        ),
+      ),
+    );
+  }
 }
